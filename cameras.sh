795718d930293561a0d17724dd835099971e16bd
Thiago Condé <thiagocondiolov@hotmail.com>
2022-04-24 20:06:22 -0300

    Fecha o script ao fechar VLC, bem melhor e mais facilgp 3

--- cameras.sh
+++ cameras.sh
@@ -7,30 +7,35 @@
 # Info   : Fazer o monitoramento das cameras motorola pelo linux, sucesso!!
 # Requis.: VLC instalado. Config: Preferencias / Interface / Marque instancia unica!!
 # Agrad. : Grupo telegram shellbr (@rbgarga)
+# Atualização: Ao fechar o VLC fecho as cameras..
 ###################################################################################
-source /home/thiago/Documentos/_imp/_scripts/senha_dvr
-# login="seu_login_DVR"
-# senha="sua_senha_DVR"
-ip="10.0.0.143"
+source /home/thiago/Documentos/_imp/_scripts/senha_dvr # comente essa linha, descomente as linhas abaixo;
+# login="seu_login_DVR" # descomente e coloque seu login
+# senha="sua_senha_DVR" # descomente e coloque sua senha
+#    ip="192.168.0.2"
 porta="554"
 
-
-pai="$(pgrep cameras.sh)"
-vlc=$(pgrep vlc)
-
-if [ ${#vlc} -gt 4 ]; then # fecho o vlc se estiver aberto
-	pkill -9 -e -f vlc
-fi
-
-if [ ${#pai} -gt 6 ]; then # fecho o script se estiver aberto e paro o script
-	pkill -9 -e -f cameras.sh
-	exit 0
-fi
+# pai="$(pgrep cameras.sh)"
+# vlc=$(pgrep vlc)
+#
+# if [ ${#vlc} -gt 4 ]; then # fecho o vlc se estiver aberto
+# 	pkill -9 -e -f vlc
+# fi
+#
+# if [ ${#pai} -gt 6 ]; then # fecho o script se estiver aberto e paro o script
+# 	pkill -9 -e -f cameras.sh
+# 	exit 0
+# fi
 
 while true; do
 	#   cvlc  -v -R rtsp://$login:$senha@$ip:$porta/h264?ch=1 & sleep 10
-	vlc v4l2 rtsp://$login:$senha@$ip:$porta/h264?ch=1 &>/dev/null &
-	sleep 10
-	#     vlc v4l2   rtsp://$login:$senha@$ip:$porta/h264?ch=4  &> /dev/null & sleep 10
+	vlc v4l2 rtsp://$login:$senha@$ip:$porta/h264?ch=1 &>/dev/null & sleep 57
+#   vlc v4l2   rtsp://$login:$senha@$ip:$porta/h264?ch=4  &> /dev/null & sleep 10
+
+	if ! pgrep -x "vlc" >/dev/null; then #verifico se o vlc esta fechado e fecho o script
+		kdialog --passivepopup 'Camera fechadas com sucesso!!'
+		pkill -9 -e -f cameras.sh
+	fi
+
 done
 exit
